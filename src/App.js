695c4f7fbefaf7e27dd7274c9b7c0ed6baa2f626
Eric Vuu <ericlv91@gmail.com>
2021-07-09 08:46:29 -0700

    loaded gltf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import './App.scss';
-import Header from './components/header';
+import React, { Suspense} from "react";
+import "./App.scss";
+import Header from "./components/header";
+
+
+import { Canvas} from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./scene.gltf");
+  return <primitive object={gltf.scene} scale={1} />;
+};
+
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight position={[0, 10, 0]} intensity={1.5} />
+      <spotLight position={[1000, 0, 0]} intensity={1} />
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Canvas>
+        <Lights />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
