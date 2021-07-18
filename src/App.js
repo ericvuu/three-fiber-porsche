c308689eea6298bdd4b3e273746567d7c524d82c
Eric Vuu <ericlv91@gmail.com>
2021-07-18 09:21:08 -0700

    added rotation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React, { Suspense} from "react";
+import React, { Suspense, useRef} from "react";
 import "./App.scss";
 import Header from "./components/header";
 import {Section} from "./components/section";
 
-import { Html } from "@react-three/drei";
+import { Html, OrbitControls } from "@react-three/drei";
 
-import { Canvas} from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
+import { Canvas, useFrame} from "@react-three/fiber";
+import { useLoader} from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const Model = () => {
+
+const PorscheModel = () => {
   const gltf = useLoader(GLTFLoader, "./scene.gltf");
   return <primitive object={gltf.scene}/>;
 };
 
+
 const HtmlContent = () => {
+
+  const ref = useRef()
+
+  useFrame(() => (ref.current.rotation.y += 0.003))
+
   return (
     <Section factor={1.5} offset={1}>
       <group position={[0, 250, 0]}>
-        <mesh position={[0,-10,0]} scale={25}>
-          <Model/>
+        <mesh ref={ref} position={[0, -10, 0]} scale={25}>
+          <PorscheModel />
         </mesh>
         <Html fullscreen>
           <div className="container">
             <h1 className="title">Feel the power</h1>
           </div>
         </Html>
+        {/* <OrbitControls
+          autoRotate
+          enablePan={false}
+          enableZoom={false}
+          enableDamping
+          dampingFactor={0.5}
+          rotateSpeed={1}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        /> */}
       </group>
     </Section>
   );
@@ -46,8 +63,8 @@ function App() {
   return (
     <>
       <Header />
-      <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 70 }}>
-        <Lights/>
+      <Canvas camera={{ position: [0, 0, 120], fov: 70 }}>
+        <Lights />
         <Suspense fallback={null}>
           <HtmlContent />
         </Suspense>
